6872e3225700ebcdd52d96c2936dcf2ba89f9de8
Aliein alin <aalin@gmail.com>
2019-06-18 10:49:43 +0800

    Aliein

---
+++ src/lifeGame/MapTest.java
@@ -0,0 +1,39 @@
+package lifeGame;
+
+import static org.junit.Assert.*;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class MapTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Test
+	public void testInitial() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetLiving() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUpdate() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testPrintMap() {
+		fail("Not yet implemented");
+	}
+
+}
